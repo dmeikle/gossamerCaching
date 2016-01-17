cb58837d1a68847e22baf9e9e51c97d017d8d02f
Dave Meikle <davemeikle@ymail.com>
2016-01-16 20:28:38 -0800

    added check for dogpile cache before deleting

--- src/Gossamer/Caching/CacheManager.php
+++ src/Gossamer/Caching/CacheManager.php
@@ -2,7 +2,6 @@
 
 namespace Gossamer\Caching;
 
-
 use Gossamer\Caching\CachingInterface;
 use Monolog\Logger;
 use Gossamer\Caching\Exceptions\FileNotFoundException;
@@ -13,222 +12,300 @@ use Gossamer\Caching\Exceptions\IOException;
  *
  * @author Dave Meikle
  */
-class CacheManager implements CachingInterface{
-   
+class CacheManager implements CachingInterface {
+
     protected $MAX_FILE_LIFESPAN = 1200;
-    
     protected $MAX_WRITE_TIME_ELAPSED = 60;
-    
     protected $logger = null;
-    
+
     const LOAD_STATIC_FILE = true;
-    
     const LOAD_PHP_FILE = false;
-    
+
     public function __construct(Logger $logger = null, array $params = null) {
         $this->logger = $logger;
-        if(!is_null($params)) {
-            if(array_key_exists('MAX_FILE_LIFESPAN', $params)) {
+        if (!is_null($params)) {
+            if (array_key_exists('MAX_FILE_LIFESPAN', $params)) {
                 $this->MAX_FILE_LIFESPAN = $params['MAX_FILE_LIFESPAN'];
             }
         }
     }
-    
+
     /**
      * since some cached values need to be re-cached if the database updates
-     * this method can be called from a save routine's post save 
+     * this method can be called from a save routine's post save
      * event handling
-     * 
+     *
      * @param string $key - the file to remove
      */
     public function invalidateCache($key) {
-        if(file_exists(__CACHE_DIRECTORY . "$key.cache")) {            
-            unlink(__CACHE_DIRECTORY . "$key.cache");          
+        if (file_exists(__CACHE_DIRECTORY . "$key.cache")) {
+            unlink(__CACHE_DIRECTORY . "$key.cache");
         }
     }
-    
+
+    /**
+     * retrieves the file from cache
+     *
+     * @param string $key
+     * @param boolean $static - is it HTML or PHP to execute?
+     *
+     * @return boolean
+     */
     public function retrieveFromCache($key, $static = false) {
         //in case the developer has added a subfolder, we need to know this
         $path = $this->buildCompletePath(__CACHE_DIRECTORY, $key);
         $key = $this->parseKey($key);
-        
-        if(file_exists($path . "$key.cache") && $this->isNotStale($path . "$key.cache", $this->MAX_FILE_LIFESPAN)) {
-            if(!$static) {
+
+        if (file_exists($path . "$key.cache") && $this->isNotStale($path . "$key.cache", $this->MAX_FILE_LIFESPAN)) {
+            if (!$static) {
                 $loadedValues = include $path . "$key.cache";
-                
-                return $loadedValues; 
+
+                return $loadedValues;
             }
-            
+
             return file_get_contents($path . "$key.cache");
         }
-        
+
         return false;
     }
-    
+
+    /**
+     * determines whether a file has outlived its decay time
+     *
+     * @param string $filepath
+     * @param string $decayTime
+     *
+     * @return boolean
+     */
     private function isNotStale($filepath, $decayTime) {
         $filetime = filemtime($filepath);
         $currentTime = time();
-        
+
         return ($currentTime - $filetime) < $decayTime;
     }
 
+    /**
+     * builds the filepath for caching files
+     *
+     * @param string $path
+     * @param string $key
+     *
+     * @return string
+     */
     private function buildCompletePath($path, $key) {
         $pieces = explode(DIRECTORY_SEPARATOR, $key);
-        if(count($pieces) == 1){
+        if (count($pieces) == 1) {
             return $path;
         }
-        
+
         array_pop($pieces);
-        
-        return $path . implode(DIRECTORY_SEPARATOR, $pieces) . DIRECTORY_SEPARATOR;        
+
+        return $path . implode(DIRECTORY_SEPARATOR, $pieces) . DIRECTORY_SEPARATOR;
     }
-    
+
+    /**
+     * gets the key for cache file names
+     *
+     * @param string $key
+     *
+     * @return string
+     */
     private function parseKey($key) {
         $pieces = explode(DIRECTORY_SEPARATOR, $key);
-        if(count($pieces) == 1){
+        if (count($pieces) == 1) {
             return $key;
         }
-                
-        
-        return array_pop($pieces); 
+
+
+        return array_pop($pieces);
     }
-    
+
     /**
-     * 
+     * deletes the cache file
+     *
      * @param string $key
      */
     public function deleteCache($key) {
-        if(file_exists(__CACHE_DIRECTORY . "$key.cache")) {
+        if (file_exists(__CACHE_DIRECTORY . "$key.cache")) {
             unlink(__CACHE_DIRECTORY . "$key.cache");
         }
     }
-    
+
+    /**
+     * saves the file to cache directory
+     *
+     * @param string $key
+     * @param string|array $values
+     * @param boolean $static
+     *
+     * @return boolean
+     */
     public function saveToCache($key, $values, $static = false) {
         //in case the developer has added a subfolder, we need to know this
         $path = $this->buildCompletePath(__CACHE_DIRECTORY, $key);
         $key = $this->parseKey($key);
-        
-        if($this->inDogpileMode($key)) {
+
+        if ($this->inDogpileMode($key)) {
             //check to see if we're in a stale write condition
-            if($this->isNotStale($path . "$key.cache.dogpile", $this->MAX_WRITE_TIME_ELAPSED)) {
+            if ($this->isNotStale($path . "$key.cache.dogpile", $this->MAX_WRITE_TIME_ELAPSED)) {
                 //someone is already writing to the file so we cannot cache right now
                 return;
             }
             //seems to be stale - shouldn't have taken this long to create
         }
 
-        try{
+        try {
             $this->verifyPathExists($path);
-    
+
             //first save current cache to dogpile file
             $this->createDogpileFile($key);
-   
-            if(!is_null($this->logger)) {
+
+            if (!is_null($this->logger)) {
                 //$this->logger->addDebug('Caching - saving values to cache file');
             }
 
             $file = fopen($path . "$key.cache", "w") or die("Unable to write to cache file to $path");
+        } catch (\Exception $e) {
+            if (!is_null($this->logger)) {
+                // $this->logger->addError($e->getMessage());
+            }
 
-        }  catch (\Exception $e) {
-            if(!is_null($this->logger)) {
-               // $this->logger->addError($e->getMessage());
-            }            
-           
             return false;
         }
-        
-        if($static) {
+
+        if ($static) {
             @fwrite($file, $values);
         } else {
             @fwrite($file, $this->formatValuesBeforeSaving($values));
         }
-        
-        fclose($file);        
-        
+
+        fclose($file);
+
         $this->deleteDogpileFile($key);
-        
+
         return true;
     }
 
+    /**
+     * checks to make sure the filepath exists
+     *
+     * @param string $path
+     *
+     * @return boolean
+     */
     protected function verifyPathExists($path) {
-        if(file_exists($path)) {
+        if (file_exists($path)) {
             return;
         }
-        
+
         $this->mkdir($path);
     }
-    
+
+    /**
+     * creates a temp file for other processes to access (as cache) while the
+     * new cache file is being generated and written to
+     *
+     * @param string $key
+     *
+     * @return void
+     */
     protected function createDogpileFile($key) {
-        if(!is_null($this->logger)) {
-           // $this->logger->addDebug('Caching - creating shunt for dogpile condition');    
+        if (!is_null($this->logger)) {
+            // $this->logger->addDebug('Caching - creating shunt for dogpile condition');
         }
-        
-        if(!file_exists(__CACHE_DIRECTORY . "$key.cache")) {
+
+        if (!file_exists(__CACHE_DIRECTORY . "$key.cache")) {
             touch(__CACHE_DIRECTORY . "$key.cache.dogpile");
-            
+
             return;
         }
-        
+
         $this->copy(__CACHE_DIRECTORY . "$key.cache", __CACHE_DIRECTORY . "$key.cache.dogpile");
     }
-    
+
+    /**
+     * removes the dogpile file once the cache writing is complete
+     *
+     * @param string $key
+     */
     protected function deleteDogpileFile($key) {
-        if(!is_null($this->logger)) {
-          // $this->logger->addDebug('Caching - deleting shunt for dogpile condition');     
+        if (!is_null($this->logger)) {
+            // $this->logger->addDebug('Caching - deleting shunt for dogpile condition');
         }
-        
-        unlink(__CACHE_DIRECTORY . "$key.cache.dogpile");
+        if (file_exists(__CACHE_DIRECTORY . "$key.cache.dogpile")) {
+            unlink(__CACHE_DIRECTORY . "$key.cache.dogpile");
+        }
     }
-    
+
+    /**
+     * lets us know if we are currently writing to a cache file, so other
+     * processes know to use the dogpile file
+     *
+     * @param string $key
+     *
+     * @return boolean
+     */
     protected function inDogpileMode($key) {
-        if(!is_null($this->logger)) {
-           //$this->logger->addDebug('Caching - checking for dogpile condition');     
+        if (!is_null($this->logger)) {
+            //$this->logger->addDebug('Caching - checking for dogpile condition');
         }
-        
-        if(file_exists(__CACHE_DIRECTORY . "$key.cache.dogpile")) {
-            if(!is_null($this->logger)) {
-               // $this->logger->addDebug('Caching - currently in dogpile condition');  
+
+        if (file_exists(__CACHE_DIRECTORY . "$key.cache.dogpile")) {
+            if (!is_null($this->logger)) {
+                // $this->logger->addDebug('Caching - currently in dogpile condition');
             }
-                      
         }
-        
+
         return file_exists(__CACHE_DIRECTORY . "$key.cache.dogpile");
     }
-    
-    
+
+    /**
+     * formats values before writing to cache
+     *
+     * @param string|array $values
+     *
+     * @return string|array
+     */
     private function formatValuesBeforeSaving($values) {
-        if(!is_null($this->logger)) {
-            //$this->logger->addDebug('Caching - formatting values before saving');     
+        if (!is_null($this->logger)) {
+            //$this->logger->addDebug('Caching - formatting values before saving');
         }
-        
-        
-        if(is_array($values)) {
+
+
+        if (is_array($values)) {
             return "<?php\r\n"
-            . "return " . $this->parseArray($values) . ";";
+                    . "return " . $this->parseArray($values) . ";";
         }
-        
+
         return $values;
     }
-    
+
+    /**
+     * parses each element to make an 'array' into a writable string
+     * 
+     * @param array $values
+     *
+     * @return string
+     */
     private function parseArray(array $values) {
-        if(!is_null($this->logger)) {
-           // $this->logger->addDebug('Caching - parsing array values');
+        if (!is_null($this->logger)) {
+            // $this->logger->addDebug('Caching - parsing array values');
         }
-        
+
         $retval = "array (";
         $elements = '';
-        foreach($values as $key => $row) {
-            if(is_array($row)) {
-                $elements .= ",\r\n'$key' => " . $this->parseArray($row) ;
-            }else{
-               $elements .= ",\r\n'$key' => '$row'"; 
-            }            
+        foreach ($values as $key => $row) {
+            if (is_array($row)) {
+                $elements .= ",\r\n'$key' => " . $this->parseArray($row);
+            } else {
+                $elements .= ",\r\n'$key' => '$row'";
+            }
         }
         $retval .= substr($elements, 1) . ")";
-        
+
         return $retval;
     }
-    
+
     /**
      * Copies a file.
      *
@@ -243,8 +320,7 @@ class CacheManager implements CachingInterface{
      * @throws FileNotFoundException    When originFile doesn't exist
      * @throws IOException              When copy fails
      */
-    public function copy($originFile, $targetFile, $override = false)
-    {
+    public function copy($originFile, $targetFile, $override = false) {
         if (stream_is_local($originFile) && !is_file($originFile)) {
             throw new FileNotFoundException(sprintf('Failed to copy "%s" because file does not exist.', $originFile), 0, null);
         }
@@ -258,7 +334,7 @@ class CacheManager implements CachingInterface{
         }
 
         if ($doCopy) {
-          
+
             $source = fopen($originFile, 'r');
             // Stream context created to allow files overwrite when using FTP stream wrapper - disabled by default
             $target = fopen($targetFile, 'w', null, stream_context_create(array('ftp' => array('overwrite' => true))));
@@ -272,8 +348,7 @@ class CacheManager implements CachingInterface{
             }
         }
     }
-    
-    
+
     /**
      * Creates a directory recursively.
      *
@@ -282,8 +357,7 @@ class CacheManager implements CachingInterface{
      *
      * @throws IOException On any directory creation failure
      */
-    public function mkdir($dirs, $mode = 0777)
-    {
+    public function mkdir($dirs, $mode = 0777) {
         foreach ($this->toIterator($dirs) as $dir) {
             if (is_dir($dir)) {
                 continue;
@@ -301,19 +375,18 @@ class CacheManager implements CachingInterface{
             }
         }
     }
-    
-    
+
     /**
      * @param mixed $files
      *
      * @return \Traversable
      */
-    private function toIterator($files)
-    {
+    private function toIterator($files) {
         if (!$files instanceof \Traversable) {
             $files = new \ArrayObject(is_array($files) ? $files : array($files));
         }
 
         return $files;
     }
+
 }
